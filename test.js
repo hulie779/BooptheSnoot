cfcf5a36134d3ee88b1c6290784c44ac4e0fabd2
chenawd <chenawd@bc.edu>
2018-01-27 16:40:57 -0500

    actual prototype update

--- test.js
+++ test.js
@@ -1,24 +1,113 @@
-class MyHomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home',
-  }
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-  render() {
-    return (
+const HomeScreen = ({navigation}) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>Boop that Snoot!</Text>
+     <Button
+       onPress={() => navigation.navigate('Image')}
+       title="Upload pet portrait"
+     />
+     <Button
+       onPress={() => navigation.navigate('Quote')}
+       title="Add some love"
+     />
+     <Button
+       onPress={() => navigation.navigate('Generate')}
+       title="Where's your boop?"
+     />
+   </View>
+);
+
+const ImageUpload = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>Uploading a picture of your lovely pet!</Text> // need a cuter rephrase
+// take care of uploading images.
+// Restrictions apply: needs to be xyz size,
+// needs to be an actual pet so nothing inappropriate gets uploaded
+// Have user plant where the boop is
+     <Button
+       onPress={() => navigation.navigate('Home')}
+       title="____" //cute name equivalent of "going back to home to keep uploading quote"
+     />
+   </View>
+);
+
+const QuoteUpload = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>Insert a heartwarmign quote!</Text> // need a cuter rephrase
+  // take care of uploading quotes.
+// Restrictions apply: needs to be xyz size,
+//needs to be actually heartwarming so nothing inappropriate gets uploaded
+  // determine where the quote shows up after the boop
+     <Button
+       onPress={() => navigation.navigate('Home')}
+       title="____" //cute name equivalent of "going back to home to keep doing xyz"
+     />
+   </View>
+);
+
+const BoopStyler = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>Stylize the boop!</Text> // need a cuter rephrase
+  // provide different themes for different boop
+// like "hope work is doing well!" or "take care of yourself!"
+     <Button
+       onPress={() => navigation.navigate('Home')}
+       title="____" //cute name equivalent of "going back to home to keep doing xyz"
+     />
+   </View>
+);
+
+const BoopScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>Boop it!</Text>
       <Button
-        onPress={() => this.props.navigation.navigate('Profile', {name: 'Lucy'})}
-        title="Go to Lucy's profile"
+        onPress={() => //share the boop}
       />
-    );
-  }
-}
+     <Button
+       onPress={() => navigation.navigate('Home')}
+       title="New boop" // generate new boop!
+     />
+     <Button
+     <Button
+       onPress={() => //re-boop}
+     />
+   </View>
+);
 
-const ModalStack = StackNavigator({
+const RootNavigator = StackNavigator({
   Home: {
-    screen: MyHomeScreen,
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
   },
-  Profile: {
-    path: 'people/:name',
-    screen: MyProfileScreen,
+  Image: {
+    screen: ImageUpload,
+    navigationOptions: {
+      headerTitle: 'Image',
+    },
+  },
+  Quote: {
+    screen: QuoteUpload,
+    navigationOptions: {
+      headerTitle: 'Quote',
+    },
+  },
+  Style: {
+    screen: BoopStyler,
+    navigationOptions: {
+      headerTitle: 'Style',
+    },
+  },
+  Boop: {
+    screen: BoopScreen,
+    navigationOptions: {
+      headerTitle: 'Boop!',
+    },
   },
 });
+
+export default RootNavigator;
